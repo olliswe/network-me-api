9103bfcaa7888437b1a43fcd218bcecf10d41263
olliswe <oliveriyer@gmail.com>
2020-01-21 07:02:27 +0000

    added post save hook

--- accounts/models.py
+++ accounts/models.py
@@ -1,13 +1,22 @@
 from django.db import models
-from django.contrib.auth.models import (
-    BaseUserManager, AbstractBaseUser
-)
+from django.contrib.auth.models import BaseUserManager, AbstractBaseUser
+from django.conf import settings
+from django.db.models.signals import post_save
+from django.dispatch import receiver
+from rest_framework.authtoken.models import Token
 
-#todo add token receiver
+# todo add token receiver
+@receiver(post_save, sender=settings.AUTH_USER_MODEL)
+def create_auth_token(sender, instance=None, created=False, **kwargs):
+    if created:
+        Token.objects.create(user=instance)
 
 
 class Category(models.Model):
-    name = models.CharField(default=None,null=True, blank=True,max_length=225,unique=True)
+    name = models.CharField(
+        default=None, null=True, blank=True, max_length=225, unique=True
+    )
+
     def __str__(self):
         return self.name
 
@@ -18,11 +27,9 @@ class UserManager(BaseUserManager):
         Creates and saves a User with the given email and password.
         """
         if not email:
-            raise ValueError('Users must have an email address')
+            raise ValueError("Users must have an email address")
 
-        user = self.model(
-            email=self.normalize_email(email),
-        )
+        user = self.model(email=self.normalize_email(email))
         user.set_password(password)
         user.save(using=self._db)
         return user
@@ -31,10 +38,7 @@ class UserManager(BaseUserManager):
         """
         Creates and saves a staff user with the given email and password.
         """
-        user = self.create_user(
-            email,
-            password=password,
-        )
+        user = self.create_user(email, password=password)
         user.staff = True
         user.save(using=self._db)
         return user
@@ -43,10 +47,7 @@ class UserManager(BaseUserManager):
         """
         Creates and saves a superuser with the given email and password.
         """
-        user = self.create_user(
-            email,
-            password=password,
-        )
+        user = self.create_user(email, password=password)
         user.staff = True
         user.admin = True
         user.save(using=self._db)
@@ -54,33 +55,44 @@ class UserManager(BaseUserManager):
 
 
 class User(AbstractBaseUser):
-    email = models.EmailField(
-        verbose_name='email address',
-        max_length=255,
-        unique=True,
+    email = models.EmailField(verbose_name="email address", max_length=255, unique=True)
+    first_name = models.CharField(
+        max_length=225, verbose_name="First Name", null=True, blank=True
     )
-    first_name = models.CharField(max_length = 225, verbose_name="First Name", null=True, blank=True)
-    last_name = models.CharField(max_length=225, verbose_name="Last Name", null=True, blank=True)
-    organization = models.CharField(max_length=225, verbose_name="Organization Name", null=True, blank=True)
-    telephone_number = models.CharField(max_length=225, verbose_name="Telephone Number", null=True, blank=True)
-    category = models.ForeignKey(Category, verbose_name = "User Category", on_delete=models.CASCADE, null=True, blank=True)
-    date_added = models.DateTimeField(auto_now_add=True,)
-    avatar = models.ImageField(verbose_name='Avatar', default='avatars/placeholder.png', upload_to='avatars/', null=True, blank=True)
+    last_name = models.CharField(
+        max_length=225, verbose_name="Last Name", null=True, blank=True
+    )
+    organization = models.CharField(
+        max_length=225, verbose_name="Organization Name", null=True, blank=True
+    )
+    telephone_number = models.CharField(
+        max_length=225, verbose_name="Telephone Number", null=True, blank=True
+    )
+    category = models.ForeignKey(
+        Category,
+        verbose_name="User Category",
+        on_delete=models.CASCADE,
+        null=True,
+        blank=True,
+    )
+    date_added = models.DateTimeField(auto_now_add=True)
+    avatar = models.ImageField(
+        verbose_name="Avatar",
+        default="avatars/placeholder.png",
+        upload_to="avatars/",
+        null=True,
+        blank=True,
+    )
     active = models.BooleanField(default=True)
     staff = models.BooleanField(default=False)  # a admin user; non super-user
     admin = models.BooleanField(default=False)  # a superuser
     # notice the absence of a "Password field", that's built in.
 
-
-
-    USERNAME_FIELD = 'email'
+    USERNAME_FIELD = "email"
     REQUIRED_FIELDS = []  # Email & Password are required by default.
-
-
 
     def __str__(self):  # __unicode__ on Python 2
         return self.email
-
 
     def has_perm(self, perm, obj=None):
         "Does the user have a specific permission?"
@@ -107,10 +119,4 @@ class User(AbstractBaseUser):
         "Is the user active?"
         return self.active
 
-
-
-
-
     objects = UserManager()
-
-
