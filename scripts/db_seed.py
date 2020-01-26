c57ab0f4c9e67ce8ea2fc27d8f436821efb0143c
olliswe <oliveriyer@gmail.com>
2020-01-26 17:34:58 +0000

    added user faker

--- scripts/db_seed.py
+++ scripts/db_seed.py
@@ -12,17 +12,14 @@ employer_cat = Category.objects.get(id=2)
 
 fake_users = []
 
-### creating 50 employers
-# for _ in range(50):
-#     user = User.objects.create_user(
-#         email = fake.email(),
-#         password = 'password123'
-#     )
-#     user.category = employer_cat
-#     user.telephone_number = fake.phone_number()
-#     user.organization = fake.company()
-#     user.save()
-#     fake_users.append(user)
+## creating 50 employers
+for _ in range(50):
+    user = User.objects.create_user(email=fake.email(), password="password123")
+    user.category = employer_cat
+    user.telephone_number = fake.phone_number()
+    user.organization = fake.company()
+    user.save()
+    fake_users.append(user)
 
 
 for employer in User.objects.filter(category=employer_cat):
