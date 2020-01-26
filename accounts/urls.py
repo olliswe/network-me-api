d379995840762cbccff28448625ddae6f2fe128a
olliswe <oliveriyer@gmail.com>
2020-01-26 13:40:25 +0000

    added login redirect

--- accounts/urls.py
+++ accounts/urls.py
@@ -2,13 +2,20 @@ from django.conf.urls import url, include
 from rest_framework import routers
 from .views import UserViewSet, current_user
 from django.urls import path
+from django.shortcuts import redirect
 
 router = routers.DefaultRouter()
 router.register(r"users", UserViewSet)
+
+
+def redirect_to_network_me(request):
+    return redirect("https://networkmesl-app.firebaseapp.com/login")
+
 
 urlpatterns = [
     url(r"", include(router.urls)),
     url(r"^auth/", include("rest_auth.urls")),
     path("current_user/", current_user),
+    path("login/", redirect_to_network_me),
     path("services/", include("django.contrib.auth.urls")),
 ]
